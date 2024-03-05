29a48ee7b278d6dcff338c59b8c6c30671dd7ed8
Nho89 <noa1720@gmail.com>
2024-03-05 14:59:29 +0100

    PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import express from  "express";
 const app = express();
 
 app.get(`/`,(req,res)=>{
-  res.send("Hello World")
+  res.send("Hello Girls")
 })
 
 try {
@@ -20,5 +20,5 @@ try {
   }
 
   app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port http.http://localhost:${PORT}`)
   })

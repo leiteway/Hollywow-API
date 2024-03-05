15389b87517fa1b0316b962617d04a82fc760fe6
Nho89 <noa1720@gmail.com>
2024-03-05 14:25:33 +0100

    synchronized

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import 'dotenv/config';
 import connection_db from "./database/connection_db.js";
+import Poster from "./models/PosterModel.js"
 
-//require('dotenv').config();
+
+
 try {
     await connection_db.authenticate();
     console.log('✅ Connection has been established successfully.');
+    await Poster.sync();
+    console.log(`synchronized successfully`)
   } catch (error) {
     console.error('❌ Unable to connect to the database:', error);
   }

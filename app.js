ccfae44b06b83cfaf87f39703ea9a46ff725dfab
Nho89 <noa1720@gmail.com>
2024-03-05 14:50:50 +0100

    function add PORT

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import 'dotenv/config';
+import {PORT} from "./config.js"
 import connection_db from "./database/connection_db.js";
 import Poster from "./models/PosterModel.js"
+import express from  "express";
 
+const app = express();
 
+app.get(`/`,(req,res)=>{
+  res.send("Hello World")
+})
 
 try {
     await connection_db.authenticate();
@@ -12,3 +18,7 @@ try {
   } catch (error) {
     console.error('❌ Unable to connect to the database:', error);
   }
+
+  app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+  })

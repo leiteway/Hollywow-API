a5b662b79a4f4b3215f2c19aa0881abd2643df65
Nho89 <noa1720@gmail.com>
2024-03-12 23:10:20 +0100

    validator delete

--- routes/PosterRouter.js
+++ routes/PosterRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { getAllPosters, deletePoster, createPoster, updatePoster, getPosterById } from "../controllers/PosterController.js";
-import {validatePoster} from "../validators/posterValidator.js"
-import {validateResult} from "../helpers/validatorHelper.js"
+import {validateCreatePoster, validateDeletePoster} from "../validators/posterValidator.js"
 const router = express.Router();
   
 router.get("/", getAllPosters);
-router.delete("/:id", deletePoster);
-router.post("/", validatePoster,validateResult, createPoster);
+router.delete("/:id",validateDeletePoster, deletePoster);
+router.post("/", validateCreatePoster,createPoster);
 router.put("/:id", updatePoster);
 router.get("/:id", getPosterById);
 export default router;

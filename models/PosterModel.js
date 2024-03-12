44596dcabc7a31b3d38fcd65a257b5d986adf2da
Nho89 <noa1720@gmail.com>
2024-03-12 11:23:04 +0100

    add supertest and jest

--- models/PosterModel.js
+++ models/PosterModel.js
@@ -5,7 +5,8 @@ import {body} from "express-validator"
 export const validatePoster = [
     body('name').notEmpty().withMessage('El nombre es requerido'),
     body('director').notEmpty().withMessage('Director no debe estar vacío'),
-    body('year').isInt().withMessage('Year debe tener números enteros')
+    body('year').isInt().withMessage('Year debe tener números enteros'),
+    body('imageUrl').notEmpty().withMessage('La imagen es obligatoria')
 ]
 
 

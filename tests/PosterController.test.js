e7873dc3e5adef14a936aecf98ff92c6bb426d02
DesireeCSilva <desiresilva1987@gmail.com>
2024-03-13 14:58:45 +0100

    afterAll added

--- tests/PosterController.test.js
+++ tests/PosterController.test.js
@@ -1,6 +1,7 @@
 import require  from 'express';
 import request from 'supertest';
 import { app } from '../app.js'; 
+import connection_db from '../database/connection_db.js';
 
 const api = request(app); //request nos permite hacer solicitudes a la app
 describe('Testing CRUD Posters', () => {
@@ -21,47 +22,9 @@ describe('Testing CRUD Posters', () => {
         });
         expect (typeof response.body).toBe("object");//tipo de dato que esperamos obtener
         expect (response.status).toBe(201);
-  })
-  
-
-
-
-    /* it("Debería crear un post", async()=>{
-        const res = await request(app)
-            .post('/posts')
-            .send({
-                title: "Título del Post",   
-                content: "Contenido del Post"                                    
-            });
-         expect(res.status).toBe(201);
-         expect(res.body).toHaveProperty('id'); 
-         expect(res.body.title).toEqual("Título del Post");    
-    })  */ 
-
-
-
-
-
-
-
-/* describe('PosterController', () => {
- it('should create a new poster', async () => {
-    const newPoster = {
-      name: 'Test Poster',
-      director: 'Test Director',
-      year: 2023,
-      imageUrl: 'http://example.com/image.jpg',
-    };
-
-    const response = await request(app)
-      .post('/posters') // Asegúrate de que esta ruta coincida con la ruta de tu API
-      .send(newPoster);
-
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('poster');
-    expect(response.body.poster).toHaveProperty('name', newPoster.name);
-    // Agrega más expectativas según sea necesario
- });
-
- // Agrega más pruebas para otros casos, como errores de validación, etc.
-}); */})
+  });
+  afterAll(async ()=> {
+    await connection_db.sync({force: true});
+    console.log("Every table was deleted!✌");
+  });
+});

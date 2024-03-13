261787366e0131aa022dadf635293ce8b491dc5a
Nho89 <noa1720@gmail.com>
2024-03-13 15:02:17 +0100

    modified test post

--- tests/PosterController.test.js
+++ tests/PosterController.test.js
@@ -4,16 +4,23 @@ import app from '../app.js';
 const api = request(app);
 describe('testing', () => {
 
-    /* test('should create a new poster', async()=>{
-      const  response = await api.post('/api');
-      expect(response.status).toBe(201);
+    test('should create a new poster', async()=>{
+      const  response = await api.post('/api').send(
+        {
+          name: "test",
+          director: "test",
+          year: 1989,
+          image_url:"https://image.com"
+        });
+      expect(typeof response.body).toBe('object');
+   expect(response.status).toBe(201);
 
-    }); */
+    });
 
-    test('should get posters', async() => {
+    /* test('should get posters', async() => {
       const response = await api.get('/api');
       expect(response.status).toBe(200);
-    });
+    }); */
 
     test('should deleted poster', async() =>{
       const response = await  api.delete("/api/:id");

bf29e91ff87d670ed4b45ffea0a2ab9b2df2949c
Nho89 <noa1720@gmail.com>
2024-03-13 12:32:55 +0100

    testing get

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ try {
 
   app.listen(PORT, ()=>{
     console.log(`Server is running on port http://localhost:${PORT}`)
-  })
+  });
+  export default app;
--- tests/PosterController.test.js
+++ tests/PosterController.test.js
@@ -1,49 +1,17 @@
-import require  from 'express';
 import request from 'supertest';
 import app from '../app.js'; 
 
 const api = request(app);
-describe('POST /posts', () => {
-  
+describe('testing', () => {
 
+    /* test('should create a new poster', async()=>{
+      const  response = await api.post('/api');
+      expect(response.status).toBe(201);
 
+    }); */
 
-    /* it("Debería crear un post", async()=>{
-        const res = await request(app)
-            .post('/posts')
-            .send({
-                title: "Título del Post",   
-                content: "Contenido del Post"                                    
-            });
-         expect(res.status).toBe(201);
-         expect(res.body).toHaveProperty('id'); 
-         expect(res.body.title).toEqual("Título del Post");    
-    })  */ 
-});
-
-
-
-
-
-
-/* describe('PosterController', () => {
- it('should create a new poster', async () => {
-    const newPoster = {
-      name: 'Test Poster',
-      director: 'Test Director',
-      year: 2023,
-      imageUrl: 'http://example.com/image.jpg',
-    };
-
-    const response = await request(app)
-      .post('/posters') // Asegúrate de que esta ruta coincida con la ruta de tu API
-      .send(newPoster);
-
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('poster');
-    expect(response.body.poster).toHaveProperty('name', newPoster.name);
-    // Agrega más expectativas según sea necesario
- });
-
- // Agrega más pruebas para otros casos, como errores de validación, etc.
-}); */
+    test('should get posters', async() => {
+      const response = await api.get('/api');
+      expect(response.status).toBe(200);
+    });
+  });

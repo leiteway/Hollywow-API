8323ddb238dc188aba4848a8028f137c9f20f9e5
Nho89 <noa1720@gmail.com>
2024-03-13 11:05:53 +0100

    modified helper

--- helpers/validatorHelper.js
+++ helpers/validatorHelper.js
@@ -2,8 +2,10 @@ import {validationResult} from 'express-validator'
 
 export const validateResult = (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+    if (errors.isEmpty()) {
+        next();
     }
-    next();
+    else{
+        return res.status(400).json({ errors: errors.array() });
+    } 
 };

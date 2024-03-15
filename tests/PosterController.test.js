00e8d28bc1fe731773bebd1b7016305ead847dc5
Nho89 <noa1720@gmail.com>
2024-03-15 13:31:05 +0100

    proof test

--- tests/PosterController.test.js
+++ tests/PosterController.test.js
@@ -1,29 +1,43 @@
 import request from 'supertest';
-import app from '../app.js'; 
+import {server, app} from '../app.js'; 
+import connection_db from '../database/connection_db.js'; 
+import Poster from  '../models/PosterModel.js'
 
 const api = request(app);
 describe('testing', () => {
 
+      beforeAll( () => {
+        connection_db.sync();
+    });
+
+      afterAll(async ()=>{
+        try {
+            await Poster.destroy({where: { director: "Tim Burton"}});
+        } catch (error) {
+            console.error('Error destroying poster:', error);
+        }
+        await connection_db.close();
+        server.close();
+});
+
+
     test('should create a new poster', async()=>{
       const  response = await api.post('/api').send(
         {
-          name: "test",
-          director: "test",
-          year: 1989,
-          image_url:"https://image.com"
+          "name": 'test',
+          "director": "test",
+          "year": "1989",
+          "imageUrl":"https://image.com"
         });
-      expect(typeof response.body).toBe('object');
-   expect(response.status).toBe(201);
-
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty('id'); 
+      expect(response.body.name).toBe('test');
     });
 
-    /* test('should get posters', async() => {
-      const response = await api.get('/api');
-      expect(response.status).toBe(200);
-    }); */
+  
 
     test('should deleted poster', async() =>{
       const response = await  api.delete("/api/:id");
-      expect('id');
+        expect(response.status).toBe(400);
     })
   });
